4cd04f97e008b00b240825ab5ebab79301c10758
Ilya Zablotski <zablotski2020@gmail.com>
2024-08-07 11:52:43 +0300

    Refactor GameInfoVC

--- RightOnTarget/AppDelegate.swift
+++ RightOnTarget/AppDelegate.swift
@@ -7,7 +7,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        Thread.sleep(forTimeInterval: 1.0)
         return true
     }
 
--- RightOnTarget/Controller/GameInfoVC.swift
+++ RightOnTarget/Controller/GameInfoVC.swift
@@ -5,14 +5,14 @@
 import UIKit
 
 final class GameInfoVC: UIViewController {
-    var cellIcon: UIImage = .hexGameLogo
     
-    // MARK: IBOutlets
+    var cellIcon: UIImage = .hexGameLogo
+
+    // MARK: Scene Elements
 
     @IBOutlet private var collectionView: UICollectionView!
 
-    // MARK: Scene Elements
-    
+
     private let homeButton: UIButton = {
         let button = UIButton()
         button.setAppButtonStyle(backgroundImage: "BlueHomeButton")
@@ -25,8 +25,35 @@ final class GameInfoVC: UIViewController {
         return imageView
     }()
 
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        makeConstraints()
+        bind()
+        collectionSetup()
+    }
+}
+
+private extension GameInfoVC {
+    private func collectionSetup() {
+        collectionView.dataSource = self
+        let buttomCellNib = UINib(nibName: "ButtonCell", bundle: nil)
+        collectionView.register(buttomCellNib, forCellWithReuseIdentifier: "ButtonCell")
+        let commonCellNib = UINib(nibName: "CommonCell", bundle: nil)
+        collectionView.register(commonCellNib, forCellWithReuseIdentifier: "CommonCell")
+    }
+
+    // MARK: Setup Views
+
+    private func setupView() {
+        view.addSubview(homeButton)
+        view.addSubview(infoTitle)
+    }
+
     // MARK: Constraints
-    
+
     private func makeConstraints() {
         homeButton.snp.makeConstraints { make in
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(20)
@@ -43,43 +70,19 @@ final class GameInfoVC: UIViewController {
         }
     }
 
-    // MARK: Actions
-    
+    // MARK: Bindings
+
     private func bind() {
         homeButton.addAction(UIAction(handler: { [weak self] _ in
             playSound(sound: "click", type: "wav")
             self?.dismiss(animated: true, completion: nil)
         }), for: .touchUpInside)
     }
-
-    // MARK: Setup Views
-    
-    private func setupView() {
-        view.addSubview(homeButton)
-        view.addSubview(infoTitle)
-    }
-
-    // MARK: - Lifecycle
-
-    override func loadView() {
-        super.loadView()
-        setupView()
-        makeConstraints()
-        bind()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.dataSource = self
-        let buttomCellNib = UINib(nibName: "ButtonCell", bundle: nil)
-        collectionView.register(buttomCellNib, forCellWithReuseIdentifier: "ButtonCell")
-        let commonCellNib = UINib(nibName: "CommonCell", bundle: nil)
-        collectionView.register(commonCellNib, forCellWithReuseIdentifier: "CommonCell")
-    }
 }
 
+// MARK: UICollection Datf Source
+
 extension GameInfoVC: UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { 4 }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -88,17 +91,17 @@ extension GameInfoVC: UICollectionViewDataSource {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ButtonCell", for: indexPath) as! ButtonCell
             cell.configure(topImage: .hexGameLogo, versImage: .appLogo, bottomImage: .cardsGameLogo)
             return cell
-            
+
         case 1:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommonCell", for: indexPath) as! CommonCell
             cell.configure(icon: cellIcon, topImage: .rulesHumans, versImage: .rulesGamepad, bottomImage: .rulesGears, text: "Rules")
             return cell
-            
+
         case 2:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommonCell", for: indexPath) as! CommonCell
             cell.configure(icon: cellIcon, topImage: .leaderboardButton, versImage: .leaderboardButton, bottomImage: .leaderboardButton, text: "Leaderboard")
             return cell
-            
+
         default:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommonCell", for: indexPath) as! CommonCell
             cell.configure(icon: cellIcon, topImage: .video, versImage: .video, bottomImage: .video, text: "Game Video")
